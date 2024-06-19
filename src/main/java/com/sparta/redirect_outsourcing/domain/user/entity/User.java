bb5054c5da856ed574b9f06e50f8da73bcd47f43
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-06-20 01:37:00 +0900

    feat : 유저 엔티티 구현

--- src/main/java/com/sparta/redirect_outsourcing/domain/user/entity/User.java
+++ src/main/java/com/sparta/redirect_outsourcing/domain/user/entity/User.java
@@ -1,18 +1,75 @@
 package com.sparta.redirect_outsourcing.domain.user.entity;
 
 import com.sparta.redirect_outsourcing.common.TimeStampEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Getter
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
 public class User extends TimeStampEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    @Column
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String introduce;
+
+    @Column
+    private String pictureUrl;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column
+    private UserStatusEnum userStatus;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column
+    private UserRoleEnum userRole;
+
+    @Column
+    private String refreshToken;
+
+    @Column
+    private String kakaoId;
+    /*
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Restaurant> restaurants;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Cart> carts;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Like> likes;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Follow> follows;
+
+     */
 }
