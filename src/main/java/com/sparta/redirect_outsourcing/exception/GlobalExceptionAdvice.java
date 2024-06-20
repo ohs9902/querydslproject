3b040dcdc31b6180494ddae6904c00e0af37a2d1
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-06-21 02:50:16 +0900

    feat : 유효성검사 에러 응답처리 추가

--- src/main/java/com/sparta/redirect_outsourcing/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/sparta/redirect_outsourcing/exception/GlobalExceptionAdvice.java
@@ -1,12 +1,21 @@
 package com.sparta.redirect_outsourcing.exception;
 
+import com.sparta.redirect_outsourcing.common.DataResponseDto;
 import com.sparta.redirect_outsourcing.common.MessageResponseDto;
 import com.sparta.redirect_outsourcing.common.ResponseUtils;
 import com.sparta.redirect_outsourcing.exception.custom.user.UserException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,5 +25,14 @@ public class GlobalExceptionAdvice {
     public ResponseEntity<MessageResponseDto> handleUserException(UserException e) {
         log.error("에러 메세지: ", e);
         return ResponseUtils.of(e.getResponseCodeEnum());
+    }
+
+    // Validation 에러
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DataResponseDto<List<String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errorMessageList = new ArrayList<>();
+        e.getBindingResult().getAllErrors().forEach(v -> errorMessageList.add(v.getDefaultMessage()));
+        log.error("유효성 검사 실패:\n\n{}", String.join(",\n", errorMessageList));
+        return ResponseUtils.of(HttpStatus.BAD_REQUEST, "유효성 검사 실패", errorMessageList);
     }
 }
