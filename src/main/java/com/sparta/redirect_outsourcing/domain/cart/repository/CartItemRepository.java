4772debc2ef6b666bd1ea9bf5d88f8dfb909a0b9
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-21 20:33:22 +0900

    CartItemRepository delete Query문 추가

--- src/main/java/com/sparta/redirect_outsourcing/domain/cart/repository/CartItemRepository.java
+++ src/main/java/com/sparta/redirect_outsourcing/domain/cart/repository/CartItemRepository.java
@@ -4,8 +4,12 @@ import com.sparta.redirect_outsourcing.domain.cart.entity.Cart;
 import com.sparta.redirect_outsourcing.domain.cart.entity.CartItem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Long> {
 
-    void deleteAllByMenuId(List<Long> menuIds);
+    @Modifying
+    @Query("DELETE FROM CartItem ci WHERE ci.menu.id IN : menuIds")
+    void deleteAllByMenuIdIn(List<Long> menuIds);
 }
