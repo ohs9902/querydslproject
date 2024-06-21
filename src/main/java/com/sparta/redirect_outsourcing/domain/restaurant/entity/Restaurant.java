ec7db61adf216fde114e6543f1b100b03ed820d8
hangeunhyeong <hgh7546@naver.com>
2024-06-21 11:26:21 +0900

    fix : restaurant 엔티티 변경

--- src/main/java/com/sparta/redirect_outsourcing/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/sparta/redirect_outsourcing/domain/restaurant/entity/Restaurant.java
@@ -1,10 +1,17 @@
 package com.sparta.redirect_outsourcing.domain.restaurant.entity;
 
 import com.sparta.redirect_outsourcing.common.TimeStampEntity;
+import com.sparta.redirect_outsourcing.domain.menu.entity.Menu;
+import com.sparta.redirect_outsourcing.domain.restaurant.dto.requestDto.RestaurantCreateRequestDto;
+import com.sparta.redirect_outsourcing.domain.restaurant.dto.requestDto.RestaurantUpdateRequestDto;
+import com.sparta.redirect_outsourcing.domain.review.entity.Review;
 import com.sparta.redirect_outsourcing.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Table(name = "restaurants")
@@ -15,11 +22,11 @@ public class Restaurant extends TimeStampEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @OneToMany(mappedBy = "restaurant_id",cascade = CascadeType.ALL,orphanRemoval = true)
-//    private List<Review> reviews = new ArrayList<>();
-//
-//    @OneToMany(mappedBy = "restaurant_id",cascade = CascadeType.ALL,orphanRemoval = true)
-//    private List<Menu> menus = new ArrayList<>();
+    @OneToMany(mappedBy = "restaurant_id",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
+    @OneToMany(mappedBy = "restaurant_id",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Menu> menus = new ArrayList<>();
 
     @Column(nullable = false)
     private String name;
@@ -37,18 +44,18 @@ public class Restaurant extends TimeStampEntity {
     @JoinColumn(name = "users_id")
     private User user;
 
-//    public Restaurant(RestaurantCreateRequestDto createReq) {
-//        this.name = createReq.getName();
-//        this.address = createReq.getAddress();
-//        this.category = createReq.getCategory();
-//        this.description = createReq.getDescription();
-//    }
+    public Restaurant(RestaurantCreateRequestDto createReq) {
+        this.name = createReq.getName();
+        this.address = createReq.getAddress();
+        this.category = createReq.getCategory();
+        this.description = createReq.getDescription();
+    }
 
 
-//    public void update(RestaurantUpdateRequestDto updateReq) {
-//        this.name = updateReq.getName();
-//        this.address = updateReq.getAddress();
-//        this.category = updateReq.getCategory();
-//        this.description = updateReq.getDescription();
-//    }
+    public void update(RestaurantUpdateRequestDto updateReq) {
+        this.name = updateReq.getName();
+        this.address = updateReq.getAddress();
+        this.category = updateReq.getCategory();
+        this.description = updateReq.getDescription();
+    }
 }
