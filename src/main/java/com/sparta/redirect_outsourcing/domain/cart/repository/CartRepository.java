97e79c2865b35c1337e445c0c4035a39a82874d5
bbackbbacke <157469179+bbackbbacke@users.noreply.github.com>
2024-06-21 16:23:09 +0900

    CartRepository에 deletedAllByMenuId 메서드 삭제 (CartItemRepository로 옮김)

--- src/main/java/com/sparta/redirect_outsourcing/domain/cart/repository/CartRepository.java
+++ src/main/java/com/sparta/redirect_outsourcing/domain/cart/repository/CartRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     List<CartItem> findAllByUserId(Long userId);
-    void deleteAllByMenuId(List<Long> menuIds);
+
 
 }
