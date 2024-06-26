10580e5f30992033077ff3ecf6b8676ca7e04ff3
원지연 <dnjs229@gmail.com>
2024-06-26 14:11:22 +0900

    깃허브 sync fork 테스트

--- src/main/java/com/sparta/redirect_outsourcing/domain/cart/dto/CartItemRequestDto.java
+++ src/main/java/com/sparta/redirect_outsourcing/domain/cart/dto/CartItemRequestDto.java
@@ -9,10 +9,6 @@ import lombok.Setter;
 @NoArgsConstructor
 
 public class CartItemRequestDto {
-//    private Long id;
-//    private Long cartId;
-//    private Long userId;
      private Long menusId;
     private Long quantity;
-//    private Long quantityPrice;
 }
